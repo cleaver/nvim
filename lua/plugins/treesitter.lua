82b59e4c83e5eac1c3e0ed43adc1197e20a5256a
Cleaver Barnes <295771+cleaver@users.noreply.github.com>
2025-07-20 17:22:51 -0400

    add markview as treesitter dep

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,8 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = { "OXY2DEV/markview.nvim" },
+    lazy = false,
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, { "elixir", "heex", "eex" })
