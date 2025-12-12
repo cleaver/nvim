a2696d39182bba5bb398ca746cedb71c5d4c6b51
Cleaver Barnes <295771+cleaver@users.noreply.github.com>
2025-12-12 13:13:01 -0500

    add some key mappings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "E", "$", { noremap = true })
+
+vim.keymap.set("n", "gF", function()
+  local cfile = vim.fn.expand("<cfile>")
+  local fname, lnum = cfile:match("([^:]+):(%d+)")
+  if fname and lnum then
+    vim.cmd("edit " .. fname)
+    vim.cmd(lnum)
+  else
+    vim.cmd("normal! gf")
+  end
+end, { buffer = 0, desc = "Go to file:line under cursor" })
