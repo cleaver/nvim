f2343bb134b9db9176ca49fafdbdcdf96205e22c
Cleaver Barnes <295771+cleaver@users.noreply.github.com>
2025-12-29 09:07:45 -0500

    change zen mode: darker background

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    zen = {
+      win = {
+        -- make zen mode background much darker / less visible
+        backdrop = {
+          blend = 15, -- lower = darker, higher = closer to normal bg
+        },
+      },
+    },
+  },
+}
