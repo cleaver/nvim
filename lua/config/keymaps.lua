ed6e889eeb37456905b20c8fff1f1d09fc871a82
Cleaver Barnes <295771+cleaver@users.noreply.github.com>
2026-01-20 11:09:41 -0500

    add keymaps for terminal and quick macros

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -22,3 +22,29 @@ vim.keymap.set("n", "<leader>bl", function()
   vim.fn.setreg("+", s) -- system clipboard
   vim.notify("Copied: " .. s)
 end, { desc = "Clipboard buffer file:line" })
+
+-- override position of default terminal
+
+vim.keymap.set("n", "<leader>ft", function()
+  Snacks.terminal(nil, { cwd = LazyVim.root(), win = { position = "right" } })
+end, { desc = "Terminal (Root Dir)" })
+
+-- open terminal in directory of current buffer
+vim.keymap.set("n", "<leader>bt", function()
+  local file = vim.api.nvim_buf_get_name(0)
+  local dir = file ~= "" and vim.fn.fnamemodify(file, ":p:h") or vim.loop.cwd()
+  Snacks.terminal(nil, { cwd = dir, win = { position = "right" } })
+end, { desc = "Terminal (buffer dir)" })
+
+vim.keymap.set("n", "<leader>bT", function()
+  local file = vim.api.nvim_buf_get_name(0)
+  local dir = file ~= "" and vim.fn.fnamemodify(file, ":p:h") or vim.loop.cwd()
+  Snacks.terminal(nil, { cwd = dir })
+end, { desc = "Terminal (buffer dir)" })
+
+-- Quick macro maps
+vim.keymap.set("n", "<F1>", "@a", { desc = "Play macro @a" })
+vim.keymap.set("n", "<F2>", "@b", { desc = "Play macro @b" })
+vim.keymap.set("n", "<F3>", "@c", { desc = "Play macro @c" })
+vim.keymap.set("n", "<F4>", "@d", { desc = "Play macro @d" })
+vim.keymap.set("n", "<F5>", "@q", { desc = "Play macro @q" })
